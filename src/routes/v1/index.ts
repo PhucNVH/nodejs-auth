0f11ce5b74c0d4596524322445cecc618dd4c9ae
vuthienanh <Vu_Thien_Anh@mckinsey.com>
2022-12-30 06:37:51 +0700

    clear code

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,5 @@
 import { Router } from 'express'
 
-import { authRouter } from '@/routes/v1/auth.route'
-import { userRouter } from './user.route'
-
 const router = Router()
-
-router.use('/auth', authRouter)
-router.use('/users', userRouter)
 
 export default router
--- src/routes/v1/user.route.ts
+++
@@ -1,8 +0,0 @@
-import { getUser } from '@/controllers/user.controller'
-import { auth } from '@/middlewares/auth'
-import { Router } from 'express'
-
-const router = Router()
-router.get('/', auth(), getUser)
-
-export { router as userRouter }
